e3077d3fb7f1c32ed0bcb8f285f313eb64377fbe
Riccardo van der Horst <riccardo271002@gmail.com>
2019-11-21 14:52:57 +0100

    Uitwerking van de exercise

---
+++ faculteit_V2.php
@@ -0,0 +1,13 @@
+<?php
+
+$nm = readline("Van welk getal wil je de faculteit weten?");
+
+$result = 1;
+
+while ($result <= $nm) {
+    $result++;
+
+}
+echo "De faculteit van " . $nm . " is " . $result;
+echo $nm * $result;
+?>
